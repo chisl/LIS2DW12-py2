3c7c57a922c4e96b22d6c5138a75e029d8abcbd6
chisl <info@chisl.io>
2018-02-05 19:15:48 +0100

    Initial commit

---
+++ LIS2DW12_constants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""LIS2DW12: MEMS digital output motion sensor: high-performance ultra-low-power 3-axis 'femto' accelerometer"""
+
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["ST Microelectronics"]
+__license__    = "TBD"
+__version__    = "Version 0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+
+#
+#   THIS FILE IS AUTOMATICALLY CREATED
+#    D O     N O T     M O D I F Y  !
+#
+
+class REG:
+	OUT_T = 13
+	WHO_AM_I = 15
+	CTRL1 = 32
